b1ba6d73362af39f5cdfdf635556fedf3be468a4
Vassily Krasnov <mykrasnov@gmail.com>
2014-10-01 23:20:20 +0400

    Second init.

---
+++ desktop.bundles/emil/emil.bemjson.js
@@ -0,0 +1,55 @@
+({
+    block: 'page',
+    title: 'emil',
+    head: [
+        { elem: 'css', url: '_emil.css' }
+    ],
+    scripts: [{ elem: 'js', url: '_emil.js' }],
+    content: [
+       {
+           block : 'subscriber',
+           content : [
+              {
+                block : 'subscription',
+                content: [
+                    { tag : 'h3', content : 'Subscribe to our newsletter.' },
+                    { tag : 'hr' },
+                    { tag : 'p', content : 'Subscribe to our newsletter and get exclusive details you wont find anywhere else straight to your inbox!' }
+                ]
+              },
+              {
+                block : 'email',
+                content : [
+                    {
+                        block : 'address',
+                        mix : { block : 'email', elem : 'address' },
+                        content : [
+                            {
+                                block : 'input',
+                                mods : { theme : 'normal' }
+                            },
+                            {
+                                block : 'button',
+                                text : 'subscribe'
+                            }
+                        ]
+                    },
+                    {
+                        block: 'success',
+                        content : [
+                            {
+                                elem : 'sign',
+                                tag : 'icon'
+                            },
+                            {
+                                elem : 'typemessage',
+                                tag : 'span'
+                            }
+                        ]
+                    }
+                ]
+              }
+           ]
+       }
+    ]
+})
